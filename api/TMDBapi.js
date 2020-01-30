96b3fe708dc0933d09d2c11742cd811cbbc2fb4f
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2020-01-30 22:55:47 +0100

    50% tuto react-native

---
+++ api/TMDBapi.js
@@ -0,0 +1,14 @@
+
+
+const API_TOKEN = "71e467a379bb516ece00678ad66802ff";
+
+export function getFilmsFromApiWithSearchedText (text,page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
+
+export function getImageFromApi (name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
+}
